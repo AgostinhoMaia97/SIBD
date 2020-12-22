07b5cf1eea1a71fa883f1e91aefed6108ed65f67
davidmaia77 <davidmaia.esag@gmail.com>
2020-12-22 18:58:48 +0000

    Changes Regarding session start and bug fixing

--- html/html/site/php/commentrate.php
+++ html/html/site/php/commentrate.php
@@ -12,7 +12,7 @@ if ($commentrated["number"] == NULL)
 {
     echo ("Thanks for rating the comment! You only can do this one time in each comment!"); ?>
     <form>
-    <input type="button" value="Go back!" onclick="history.back()">
+      <input type="button" value="Go back!" onclick="history.back()">
    </form>
    <?php insertCommentEvaluation($commentid, $commentrate); 
 }
@@ -20,7 +20,7 @@ if ($commentrated["number"] == NULL)
 else 
 {
 
-   echo('You already rated this post!<br><a href="../php/initialpage.php">Click here</a> to return to the main page.');
+   echo('You already rated this comment!<br><a href="../php/initialpage.php">Click here</a> to return to the main page.');
 }
 
 ?> 
